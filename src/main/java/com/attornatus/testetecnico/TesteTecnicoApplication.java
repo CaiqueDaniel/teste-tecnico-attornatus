371e8cc6a46ec0473815f4e39d0f83e131a3578a
Caique Daniel <caiquedaniel@outlook.com.br>
2023-01-08 16:50:06 -0300

    Adicionar Swagger.

--- src/main/java/com/attornatus/testetecnico/TesteTecnicoApplication.java
+++ src/main/java/com/attornatus/testetecnico/TesteTecnicoApplication.java
@@ -2,7 +2,6 @@ package com.attornatus.testetecnico;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
-import io.swagger.v3.oas.annotations.info.License;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
